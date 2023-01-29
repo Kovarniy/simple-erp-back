5ddcdc74f68810de720529f6a486a1dac39dbcbb
Evgeniy Schemelinin <Kovarniy@users.noreply.github.com>
2023-01-29 16:27:26 +0800

    добавил для роутов префикс

--- settings/employeesRouter.js
+++ settings/employeesRouter.js
@@ -1,9 +1,9 @@
 module.exports = (app) => {
     const employeesController = require('../controller/EmployeesController');
 
-    app.route("/employees/add").post(employeesController.add);
-    app.route("/employees").get(employeesController.getAll);
-    app.route("/employees/:id").get(employeesController.getOne);
-    app.route("/employees").put(employeesController.update);
-    app.route("/employees/delete/:id").delete(employeesController.delete);
+    app.route("/api/employees/add").post(employeesController.add);
+    app.route("/api/employees").get(employeesController.getAll);
+    app.route("/api/employees/:id").get(employeesController.getOne);
+    app.route("/api/employees").put(employeesController.update);
+    app.route("/api/employees/delete/:id").delete(employeesController.delete);
 }
